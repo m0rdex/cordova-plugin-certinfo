40d79a8a4513a93c46f7862b98cbfe93daec74e3
Wilson Young <yyfearth@gmail.com>
2016-12-30 15:37:59 -0800

    refine trusted for android with error message without retry connect

--- src/android/SSLCertificateChecker.java
+++ src/android/SSLCertificateChecker.java
@@ -11,6 +11,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -24,14 +25,81 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 public class SSLCertificateChecker extends CordovaPlugin {
 
   private static final String ACTION_CHECK_EVENT = "check";
   private static char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-  private static SSLSocketFactory TRUSTED_FACTORY;
-  private static HostnameVerifier TRUSTED_VERIFIER;
+
+  private static class CertificateResult {
+    private Exception exception = null;
+
+    public boolean isTrusted() {
+      return exception == null;
+    }
+
+    public Exception getException() {
+      return exception;
+    }
+
+    public void setException(Exception ex) {
+      exception = ex;
+    }
+  }
+
+  private static class TrustManagerDelegate implements X509TrustManager {
+    private CertificateResult certificateResult = null;
+    private static X509TrustManager DEFAULT_TRUST_MANAGER;
+
+    static {
+      try {
+        TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        factory.init((KeyStore) null);
+        TrustManager[] trustManagers = factory.getTrustManagers();
+        for (TrustManager tm : trustManagers) {
+          if (tm instanceof X509TrustManager) {
+            DEFAULT_TRUST_MANAGER = (X509TrustManager) tm;
+            break;
+          }
+        }
+      } catch (NoSuchAlgorithmException | KeyStoreException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public CertificateResult getCertificateResult() {
+      return certificateResult;
+    }
+
+    public void setCertificateResult(CertificateResult certificateResult) {
+      this.certificateResult = certificateResult;
+    }
+
+    @Override
+    public void checkClientTrusted(final X509Certificate[] chain, final String authType) throws CertificateException {
+      DEFAULT_TRUST_MANAGER.checkClientTrusted(chain, authType);
+    }
+
+    @Override
+    public void checkServerTrusted(final X509Certificate[] chain, final String authType) throws CertificateException {
+      try {
+        DEFAULT_TRUST_MANAGER.checkServerTrusted(chain, authType);
+      } catch (CertificateException ex) {
+        if (certificateResult == null) {
+          throw ex;
+        } else {
+          certificateResult.setException(ex);
+        }
+      }
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+      return DEFAULT_TRUST_MANAGER.getAcceptedIssuers();
+    }
+  }
 
   @Override
   public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -39,24 +107,18 @@ public class SSLCertificateChecker extends CordovaPlugin {
       cordova.getThreadPool().execute(new Runnable() {
         public void run() {
           try {
-            boolean trusted = true;
             X509Certificate cert;
             final String serverURL = args.getString(0);
             final boolean allowUntrusted = args.length() > 1 && args.getBoolean(1);
-            try {
-              cert = getCertificate(serverURL, false);
-            } catch (Exception ex) {
-              if (allowUntrusted) {
-                trusted = false;
-                cert = getCertificate(serverURL, true);
-              } else {
-                throw ex;
-              }
-            }
+            CertificateResult result = allowUntrusted ? new CertificateResult() : null;
+            cert = getCertificate(serverURL, result);
             final byte[] data = cert.getEncoded();
             final String fingerprint = getFingerprint(data);
             final JSONObject json = new JSONObject();
-            json.put("trusted", trusted);
+            json.put("trusted", result != null && result.isTrusted());
+            if (result != null && !result.isTrusted()) {
+              json.put("error", result.getException().getMessage());
+            }
             json.put("certificate", Base64.encodeToString(data, Base64.NO_WRAP));
             json.put("fingerprint", fingerprint);
             json.put("subject", cert.getSubjectX500Principal());
@@ -74,52 +136,36 @@ public class SSLCertificateChecker extends CordovaPlugin {
     }
   }
 
-  private static SSLSocketFactory getTrustedFactory() throws IOException {
-    if (TRUSTED_FACTORY == null) {
-      final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+  private static SSLSocketFactory getTrustedFactory(final CertificateResult result) throws IOException {
+    try {
+      SSLContext context = SSLContext.getInstance("TLS");
+      TrustManagerDelegate tm = new TrustManagerDelegate();
+      tm.setCertificateResult(result);
+      context.init(null, new TrustManager[]{tm}, new SecureRandom());
 
-        public X509Certificate[] getAcceptedIssuers() {
-          return new X509Certificate[0];
-        }
-
-        public void checkClientTrusted(X509Certificate[] chain, String authType) {
-          // Intentionally left blank
-        }
-
-        public void checkServerTrusted(X509Certificate[] chain, String authType) {
-          // Intentionally left blank
-        }
-      }};
-      try {
-        SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, trustAllCerts, new SecureRandom());
-
-        TRUSTED_FACTORY = context.getSocketFactory();
-      } catch (GeneralSecurityException e) {
-        throw new IOException("Security exception configuring SSL context", e);
-      }
+      return context.getSocketFactory();
+    } catch (GeneralSecurityException e) {
+      throw new IOException("Security exception configuring SSL context", e);
     }
-
-    return TRUSTED_FACTORY;
   }
 
-  private static HostnameVerifier getTrustedVerifier() {
-    if (TRUSTED_VERIFIER == null)
-      TRUSTED_VERIFIER = new HostnameVerifier() {
+  private static HostnameVerifier getTrustedVerifier(final CertificateResult result) {
+    return new HostnameVerifier() {
 
-        public boolean verify(String hostname, SSLSession session) {
-          return true;
+      public boolean verify(String hostname, SSLSession session) {
+        if (!HttpsURLConnection.getDefaultHostnameVerifier().verify(hostname, session)) {
+          result.setException(new CertificateException("Hostname not matched"));
         }
-      };
-
-    return TRUSTED_VERIFIER;
+        return true;
+      }
+    };
   }
 
-  private static X509Certificate getCertificate(String httpsURL, boolean trustAllCert) throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
+  private static X509Certificate getCertificate(String httpsURL, final CertificateResult result) throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
     final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
-    if (trustAllCert) {
-      con.setSSLSocketFactory(getTrustedFactory());
-      con.setHostnameVerifier(getTrustedVerifier());
+    if (result != null) {
+      con.setSSLSocketFactory(getTrustedFactory(result));
+      con.setHostnameVerifier(getTrustedVerifier(result));
     }
     con.setConnectTimeout(5000);
     con.connect();
