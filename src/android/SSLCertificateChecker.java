0f99641b253c8e9febd4de98f43f4545085dc61d
Wilson Young <yyfearth@gmail.com>
2016-12-30 15:01:11 -0800

    return certificate is trusted for android by retry connect

--- src/android/SSLCertificateChecker.java
+++ src/android/SSLCertificateChecker.java
@@ -8,19 +8,30 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.security.cert.CertificateException;
 import java.io.IOException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.KeyStoreException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateEncodingException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class SSLCertificateChecker extends CordovaPlugin {
 
   private static final String ACTION_CHECK_EVENT = "check";
   private static char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+  private static SSLSocketFactory TRUSTED_FACTORY;
+  private static HostnameVerifier TRUSTED_VERIFIER;
 
   @Override
   public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -28,25 +39,29 @@ public class SSLCertificateChecker extends CordovaPlugin {
       cordova.getThreadPool().execute(new Runnable() {
         public void run() {
           try {
+            boolean trusted = true;
+            X509Certificate cert;
             final String serverURL = args.getString(0);
-            //final JSONArray allowedFingerprints = args.getJSONArray(2);
-            final X509Certificate cert = getCertificate(serverURL);
+            final boolean allowUntrusted = args.length() > 1 && args.getBoolean(1);
+            try {
+              cert = getCertificate(serverURL, false);
+            } catch (Exception ex) {
+              if (allowUntrusted) {
+                trusted = false;
+                cert = getCertificate(serverURL, true);
+              } else {
+                throw ex;
+              }
+            }
             final byte[] data = cert.getEncoded();
             final String fingerprint = getFingerprint(data);
             final JSONObject json = new JSONObject();
+            json.put("trusted", trusted);
             json.put("certificate", Base64.encodeToString(data, Base64.NO_WRAP));
             json.put("fingerprint", fingerprint);
             json.put("subject", cert.getSubjectX500Principal());
             json.put("issuer", cert.getIssuerX500Principal());
-            json.put("details", cert.toString());
             callbackContext.success(json);
-            //for (int j=0; j<allowedFingerprints.length(); j++) {
-            //  if (allowedFingerprints.get(j).toString().equalsIgnoreCase(serverCertFingerprint)) {
-            //    callbackContext.success("CONNECTION_SECURE");
-            //    return;
-            //  }
-            //}
-            //callbackContext.error("CONNECTION_NOT_SECURE");
           } catch (Exception e) {
             callbackContext.error("CONNECTION_FAILED. Details: " + e.getMessage());
           }
@@ -59,14 +74,61 @@ public class SSLCertificateChecker extends CordovaPlugin {
     }
   }
 
-  private static X509Certificate getCertificate(String httpsURL) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
-    final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
-    con.setConnectTimeout(5000);
-    con.connect();
-    return (X509Certificate)con.getServerCertificates()[0];
+  private static SSLSocketFactory getTrustedFactory() throws IOException {
+    if (TRUSTED_FACTORY == null) {
+      final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+
+        public X509Certificate[] getAcceptedIssuers() {
+          return new X509Certificate[0];
+        }
+
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+          // Intentionally left blank
+        }
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+          // Intentionally left blank
+        }
+      }};
+      try {
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, trustAllCerts, new SecureRandom());
+
+        TRUSTED_FACTORY = context.getSocketFactory();
+      } catch (GeneralSecurityException e) {
+        throw new IOException("Security exception configuring SSL context", e);
+      }
+    }
+
+    return TRUSTED_FACTORY;
   }
 
-  private static String getFingerprint(final byte[] data) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
+  private static HostnameVerifier getTrustedVerifier() {
+    if (TRUSTED_VERIFIER == null)
+      TRUSTED_VERIFIER = new HostnameVerifier() {
+
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      };
+
+    return TRUSTED_VERIFIER;
+  }
+
+  private static X509Certificate getCertificate(String httpsURL, boolean trustAllCert) throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
+    final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
+    if (trustAllCert) {
+      con.setSSLSocketFactory(getTrustedFactory());
+      con.setHostnameVerifier(getTrustedVerifier());
+    }
+    con.setConnectTimeout(5000);
+    con.connect();
+    X509Certificate certificate = (X509Certificate) con.getServerCertificates()[0];
+    con.disconnect();
+    return certificate;
+  }
+
+  private static String getFingerprint(final byte[] data) throws IOException, NoSuchAlgorithmException, CertificateException {
     final MessageDigest md = MessageDigest.getInstance("SHA1");
     md.update(data);
     return dumpHex(md.digest());
