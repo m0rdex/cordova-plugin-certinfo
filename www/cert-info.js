9b310eb9cebe0908500514a9d2ef818bae93e0bc
Wilson Young <yyfearth@gmail.com>
2016-12-30 15:47:07 -0800

    add allow untrusted cert argument and verify url

--- www/cert-info.js
+++ www/cert-info.js
@@ -2,9 +2,12 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  fetch: function(serverURL) {
+  fetch: function(url, allowUntrusted) {
+    if (!url || typeof url !== 'string' || url.slice(0, 6).toLowerCase() !== 'https:') {
+      return Promise.reject(new Error('a valid url with https protocol must be given'));
+    }
     return new Promise(function(resolve, reject) {
-      exec(resolve, reject, "CertInfo", "fetch", [serverURL]);
+      exec(resolve, reject, 'CertInfo', 'fetch', [url, !!allowUntrusted]);
     });
   }
 };
